d22a4ce59e123504406919854b18734692f146f9
gustavo-gsilva <prazeresdasilvag@gmail.com>
2025-05-22 20:09:06 -0300

    chore: deploy to GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import GlobalStyle from "./styles/GlobalStyles";
 import Home from "./pages/Home";
 import PokemonDetailsPage from './pages/PokemonDetailsPage';
 
 function App() {
    return (
-      <BrowserRouter>
+      <HashRouter>
          <GlobalStyle />
          <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/pokemon/:name" element={<PokemonDetailsPage />} />
          </Routes>
-      </BrowserRouter>
+      </HashRouter>
    )
 };
 
