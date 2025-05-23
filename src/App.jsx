ebabc6a2e271796925bac510da55a9569b3b9958
gustavo-gsilva <prazeresdasilvag@gmail.com>
2025-05-22 21:37:13 -0300

    fix: correction deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
-import { Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import GlobalStyle from "./styles/GlobalStyles";
 import Home from "./pages/Home";
 import PokemonDetailsPage from './pages/PokemonDetailsPage';
 
 function App() {
    return (
-      <HashRouter>
+      <BrowserRouter basename='/pokedex'>
          <GlobalStyle />
          <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/pokemon/:name" element={<PokemonDetailsPage />} />
          </Routes>
-      </HashRouter>
+      </BrowserRouter>
    )
 };
 
